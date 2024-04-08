f78fb878c1dfef6b8b16cfb3116124f4482970e9
staaaaaaaaa <zaytsevaasya@yahoo.com>
2024-04-09 02:53:36 +0300

    хочу спать транзакции на базе

--- delivery4u.py
+++ delivery4u.py
@@ -1,12 +1,10 @@
 from inventory import *
+initial_inventory = [Product(1, "Chair", 10, 10, 50), Product(2, "Lipstick", 20, 20, 15)]
+store = Inventory(initial_inventory)
 
-# Apple = Product(1, "Apple", 50, 100)
-# Orange = Product(2, "Orange", 60, 120)
-
-# Apple.showInformation()
-
-Apple = Transaction(1, "Apple", 50, 100)
-
-Apple.addQuantity(2) 
-
-Apple.showInformation()
+store.add_new_product_to_inventory(Product(3, "Babyoil", 1, 10, 50))
+store.sell_product_stock('Babyoil', 1)
+store.restock_product('Babyoil', 15)
+store.sell_product_stock('Babyoil', 5)
+store.print_transactions_logs()
+store.print_financial_report()
--- inventory.py
+++ inventory.py
@@ -1,3 +1,6 @@
+from tabulate import tabulate
+from datetime import datetime
+
 class Product:
     def __init__(self, id, name, quantity, price_purchase, price_sell):
         self.id = id
@@ -8,53 +11,69 @@ class Product:
     
 
 class Inventory:
-    def __init__(self, stock_inventory):
-        self.stock_inventory = stock_inventory
+    def __init__(self, inventory):
+        self.inventory = inventory
         self.inventory_logs = []
 
+        self.revenue = 0
+        self.costs = 0
+
+
     def print_list_of_products(self):
-        for product in self.stock_inventory:
-            print(f"""Product: {product.name} ID: {product.id} Quantity: {product.quantity}""")
+        inventory_table = []
+        for product in self.inventory:
+            inventory_table.append({k: v for i, (k, v) in enumerate(vars(product).items()) if i < len(vars(product)) - 2})
+        print(tabulate(inventory_table, headers='keys'))
     
     def print_product_info(self, product_name):
-        for product in self.stock_inventory:
+        product_info = f'{product_name} not found in the inventory. Try again or add product.'
+        for product in self.inventory:
             if product.name == product_name:
-                print(f"""
-Product: {product.name} 
-ID: {product.id} 
-Quantity: {product.quantity}
-Purchasing price: {product.price_purchase}
-Selling price: {product.price_sell}
-""")
+                product_info = tabulate(vars(product).items(), headers=[product_name,''])
+        print(product_info)
+
+    def print_transactions_logs(self):
+        print(tabulate(self.inventory_logs, headers='keys'))
+
+    def print_financial_report(self):
+        print(tabulate([['revenue', self.revenue], ['cost', self.costs], ['profit', self.revenue - self.costs]]))
+
+
                 
-    def add_new_product_to_inventory(self, product):
-        if product.name != "":
-            self.stock_inventory.append(product)
+    def add_new_product_to_inventory(self, product_new):
+        log = f'Error while adding product. Try again.' 
+        for product in self.inventory:
+            if product.name == product.name:
+                log = f'Error while adding product. Try again.'
+        if product_new.name != "":
+            self.inventory.append(product_new)
+            log = f"Added new product {product_new.name}: {product_new.quantity}"
+            self.costs = self.costs + product_new.quantity * product_new.price_purchase
+        self.inventory_logs.append({'date': datetime.now(), 
+                                    'transaction': log})
+        print(log)
     
     def restock_product(self, product_name, added_items):
-        for product in self.stock_inventory:
+        log = f'{product_name} not found in the inventory. Try again or add product.'
+        for product in self.inventory:
             if product.name == product_name:
                 product.quantity = product.quantity + added_items
-                log = f"Added new stock for {product_name}. New value: {product.quantity}"
-                self.inventory_logs.append(log)
-                print(log)
+                log = f"Added new stock for {product_name}: {product.quantity - added_items} -> {product.quantity}"
+                self.costs = self.costs + product.quantity * product.price_purchase
+        self.inventory_logs.append({'date': datetime.now(), 
+                                'transaction': log})
+        print(log)
+        
     
     def sell_product_stock(self, product_name, sold_items):
-        for product in self.stock_inventory:
-            if product.name == product_name and sold_items < product.quantity:
+        log = f'{product_name} not found in the inventory. Try again or add product.'
+        for product in self.inventory:
+            if product.name == product_name and product.quantity >= sold_items:
                 product.quantity = product.quantity - sold_items
-                log = f"{product_name}: {product.quantity + sold_items} -> {product.quantity}"
-                self.inventory_logs.append(log)
-                print(log)
-
-    
-
-stock_inventory = [Product(1, "Chair", 10, 10, 50), Product(2, "Lipstick", 20, 20, 15)]
-store = Inventory(stock_inventory)
-
-store.add_new_product_to_inventory(Product(3, "Babyoil", 1, 1, 50))
-store.print_list_of_products()
-store.restock_product('Chair', 3)
-store.print_list_of_products()
-store.sell_product_stock('Babyoil', 3)
-store.print_list_of_products()
+                log = f"Sold {product_name}: {product.quantity + sold_items} -> {product.quantity}"
+                self.revenue = self.revenue + product.quantity * product.price_sell
+            elif product.name == product_name and product.quantity < sold_items:
+                log = f"Unsuccessful transaction. Not enough stock for sell {sold_items} of {product_name}: {product.quantity}"
+        self.inventory_logs.append({'date': datetime.now(), 
+                                'transaction': log})
+        print(log)
